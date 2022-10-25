c1e8c80d3141bf6fd66c329adae68cd78ef7818b
qianfengg <fred_gqf0221@163.com>
2022-10-25 15:58:23 +0800

    getters处理token

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,8 @@
 const getters = {
   sidebar: (state) => state.app.sidebar,
   device: (state) => state.app.device,
+  token: (state) => state.user.token,
 };
+// this.$store.state.user.token
+// this.$store.getters.token
 export default getters;
