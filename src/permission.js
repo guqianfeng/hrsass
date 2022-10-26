b2adc626a509af00e36b9a746920abe6c6f26acb
qianfengg <fred_gqf0221@163.com>
2022-10-26 10:35:43 +0800

    NProgress的使用

--- src/permission.js
+++ src/permission.js
@@ -1,12 +1,16 @@
 import router from "@/router";
 import store from "@/store";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const whiteList = ["/login", "/404"];
 router.beforeEach((to, from, next) => {
+  NProgress.start();
   const token = store.getters.token;
   if (token) {
     if (to.path === "/login") {
       next("/");
+      NProgress.done();
     } else {
       next();
     }
@@ -15,6 +19,10 @@ router.beforeEach((to, from, next) => {
       next();
     } else {
       next("/login");
+      NProgress.done();
     }
   }
+});
+router.afterEach((to, from) => {
+  NProgress.done();
 });
