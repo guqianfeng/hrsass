ae4115d08c448d7675fefbe1ed23d8c2c8ac70c5
qianfengg <fred_gqf0221@163.com>
2022-10-28 09:44:27 +0800

    组织架构基本结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="departments-container">
     <div class="app-container">
-      <el-card>
-        <el-row>
-          <el-col :span="8" style="background: pink">1111</el-col>
-          <el-col :span="8" style="background: orange">1111</el-col>
-          <el-col :span="8" style="background: skyblue">1111</el-col>
-          <el-col :span="8" style="background: pink">1111</el-col>
-          <el-col :span="8" style="background: orange">1111</el-col>
-          <el-col :span="8" style="background: skyblue">1111</el-col>
-          <el-col :span="8" style="background: pink">1111</el-col>
-          <el-col :span="8" style="background: orange">1111</el-col>
-          <el-col :span="8" style="background: skyblue">1111</el-col>
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
+          <el-col :span="20">
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex">
+              <!-- 两个内容 -->
+              <el-col :span="12">负责人</el-col>
+              <el-col :span="12">
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span>
+                    操作<i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
         </el-row>
       </el-card>
     </div>
