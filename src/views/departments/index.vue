0fcb5a75ed3acf8ed2f6f8df515f788c0084b9fd
qianfengg <fred_gqf0221@163.com>
2022-10-28 09:39:24 +0800

    布局组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="departments-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card>
+        <el-row>
+          <el-col :span="8" style="background: pink">1111</el-col>
+          <el-col :span="8" style="background: orange">1111</el-col>
+          <el-col :span="8" style="background: skyblue">1111</el-col>
+          <el-col :span="8" style="background: pink">1111</el-col>
+          <el-col :span="8" style="background: orange">1111</el-col>
+          <el-col :span="8" style="background: skyblue">1111</el-col>
+          <el-col :span="8" style="background: pink">1111</el-col>
+          <el-col :span="8" style="background: orange">1111</el-col>
+          <el-col :span="8" style="background: skyblue">1111</el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
