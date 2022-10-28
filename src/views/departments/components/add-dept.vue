88723ae338badec4e07a8847dc7c4ed2afc8067d
qianfengg <fred_gqf0221@163.com>
2022-10-28 14:32:25 +0800

    准备新增部门表单结构

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -1,14 +1,27 @@
 <template>
-  <el-dialog
-    title="温馨提示"
-    :visible="dialogVisible"
-    @close="closeDialog"
-  >
-    <span>这是一段信息</span>
-    <template #footer>
-      <el-button>取消</el-button>
-      <el-button type="primary">确定</el-button>
-    </template>
+  <!-- 新增部门的弹层 -->
+  <el-dialog title="新增部门" :visible="dialogVisible" @close="handleCloseDialog">
+    <!-- 表单组件  el-form   label-width设置label的宽度   -->
+    <!-- 匿名插槽 -->
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input style="width:80%" placeholder="1-50个字符" />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input style="width:80%" placeholder="1-50个字符" />
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <!-- <el-select style="width:80%" placeholder="请选择" /> -->
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+      </el-form-item>
+    </el-form>
+    <!-- el-dialog有专门放置底部操作栏的 插槽  具名插槽 -->
+    <div slot="footer">
+      <el-button type="primary" size="small">确定</el-button>
+      <el-button size="small">取消</el-button>
+    </div>
   </el-dialog>
 </template>
 
@@ -21,7 +34,7 @@ export default {
     }
   },
   methods: {
-    closeDialog() {
+    handleCloseDialog() {
       this.$emit('close-dialog', false)
     }
   }
