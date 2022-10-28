59883f98d0e6806bc8771cc8eea541823ee24047
qianfengg <fred_gqf0221@163.com>
2022-10-28 10:42:57 +0800

    封装tree-tool

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <el-col :span="20">
+      <span>{{ nodeData.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex">
+        <!-- 两个内容 -->
+        <el-col :span="12">{{ nodeData.manager }}</el-col>
+        <el-col :span="12">
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部分</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    nodeData: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,55 +4,11 @@
       <el-card class="tree-card">
         <!-- 头部 -->
         <!-- 用了一个行列布局 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col :span="20">
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex">
-              <!-- 两个内容 -->
-              <el-col :span="12">负责人</el-col>
-              <el-col :span="12">
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span>
-                    操作<i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :node-data="company" is-root />
         <!-- 内容 -->
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
           <template #default="{data}">
-            <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-              <el-col :span="20">
-                <span>{{ data.name }}</span>
-              </el-col>
-              <el-col :span="4">
-                <el-row type="flex">
-                  <!-- 两个内容 -->
-                  <el-col :span="12">{{ data.manager }}</el-col>
-                  <el-col :span="12">
-                    <!-- 下拉菜单 element -->
-                    <el-dropdown>
-                      <span>
-                        操作<i class="el-icon-arrow-down" />
-                      </span>
-                      <!-- 下拉菜单 -->
-                      <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>添加子部门</el-dropdown-item>
-                      </el-dropdown-menu>
-                    </el-dropdown>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
+            <tree-tools :node-data="data" />
           </template>
         </el-tree>
       </el-card>
@@ -61,10 +17,13 @@
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
   name: 'Departments',
+  components: { treeTools },
   data() {
     return {
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
       departs: [
         {
           name: '总裁办',
