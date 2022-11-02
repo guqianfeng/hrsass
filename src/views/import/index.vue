ece9c026d008c69b624391eb4a70d11ed8def37f
qianfengg <fred_gqf0221@163.com>
2022-11-02 10:25:19 +0800

    处理对象数据

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -8,7 +8,25 @@
 export default {
   methods: {
     handleSuccess({ header, results }) {
-      console.log(header, results)
+      // console.log(header, results)
+      const userRelations = {
+        '入职日期': 'timeOfEntry',
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber'
+      }
+      const arr = []
+      results.forEach(res => {
+        const obj = {}
+        // console.log(res)
+        for (const key in res) {
+          obj[userRelations[key]] = res[key]
+        }
+        // console.log(obj)
+        arr.push(obj)
+      })
+      console.log(arr)
     }
   }
 }
