cee8a9638e5d6f178519bdd5bcf7bcb2fb999a6b
qianfengg <fred_gqf0221@163.com>
2022-11-02 10:14:23 +0800

    封装批量导入员工api函数

--- src/api/employees.js
+++ src/api/employees.js
@@ -37,3 +37,16 @@ export function reqAddEmployee(form) {
     data: form
   })
 }
+
+/**
+ * 批量导入员工
+ * @param {*} arr
+ * @returns
+ */
+export function reqUserBatch(arr) {
+  return request({
+    url: '/sys/user/batch',
+    method: 'post',
+    data: arr
+  })
+}
