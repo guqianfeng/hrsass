1eca879c0d4fb0c9d7b8890a52a49977b99d6ac3
qianfengg <fred_gqf0221@163.com>
2022-11-03 20:04:24 +0800

    处理上传成功后的回调

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -65,17 +65,23 @@ export default {
       this.fileList = [...fileList]
     },
     handleRequest(e) {
-      console.log('request', e.file)
+      const file = e.file
+      const fileObj = this.fileList.find(item => item.uid === file.uid)
+      console.log(fileObj)
       cos.putObject({
         Bucket: 'fred-1308337721', /* 填入您自己的存储桶，必须字段 */
         Region: 'ap-shanghai', /* 存储桶所在地域，例如ap-beijing，必须字段 */
-        Key: +Date.now() + '_' + e.file.name, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
-        Body: e.file, /* 必须，上传文件对象，可以是input[type="file"]标签选择本地文件后得到的file对象 */
+        Key: +Date.now() + '_' + file.name, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
+        Body: file, /* 必须，上传文件对象，可以是input[type="file"]标签选择本地文件后得到的file对象 */
         onProgress: function(progressData) {
           console.log(JSON.stringify(progressData))
         }
-      }, function(err, data) {
-        console.log(err || data)
+      }, (err, data) => {
+        if (err) {
+          return false
+        }
+        fileObj.status = 'success'
+        fileObj.url = 'https://' + data.Location
       })
     },
     handleRemove(file, fileList) {
