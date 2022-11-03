76f95af72886ff7c5156d640c0e23dba5115f4d9
qianfengg <fred_gqf0221@163.com>
2022-11-03 18:22:26 +0800

    上传腾讯云

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -21,6 +21,15 @@
 </template>
 
 <script>
+// SecretId: AKIDSp11nJdpohXOzSjplTxDE1XpCMHwOefS
+// SecretKey: cy74vY5QZIn496qOhkaCf5KOacwISN4V
+
+import COS from 'cos-js-sdk-v5' // 导入腾讯云的包(sdk)
+const cos = new COS({
+  SecretId: 'AKIDSp11nJdpohXOzSjplTxDE1XpCMHwOefS', // 身份识别ID
+  SecretKey: 'cy74vY5QZIn496qOhkaCf5KOacwISN4V' // 身份秘钥
+})
+
 export default {
   name: 'ImageUpload',
   props: {
@@ -57,6 +66,17 @@ export default {
     },
     handleRequest(e) {
       console.log('request', e.file)
+      cos.putObject({
+        Bucket: 'fred-1308337721', /* 填入您自己的存储桶，必须字段 */
+        Region: 'ap-shanghai', /* 存储桶所在地域，例如ap-beijing，必须字段 */
+        Key: +Date.now() + '_' + e.file.name, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
+        Body: e.file, /* 必须，上传文件对象，可以是input[type="file"]标签选择本地文件后得到的file对象 */
+        onProgress: function(progressData) {
+          console.log(JSON.stringify(progressData))
+        }
+      }, function(err, data) {
+        console.log(err || data)
+      })
     },
     handleRemove(file, fileList) {
       // console.log(file, fileList)
