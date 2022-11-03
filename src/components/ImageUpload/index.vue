6c054e14381707e6fec94efff3d14736c9651997
qianfengg <fred_gqf0221@163.com>
2022-11-03 15:53:32 +0800

    初步封装上传组件

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="image-upload">
+    <el-upload
+      action="#"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageUpload',
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePictureCardPreview(file) {
+      console.log(file)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
