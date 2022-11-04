735858cae4d21d411039ad30df74f31035799847
qianfengg <fred_gqf0221@163.com>
2022-11-04 15:59:35 +0800

    修改权限成功

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -13,13 +13,13 @@
           <el-table-column label="操作">
             <template #default="{row}">
               <el-button v-if="row.type === 1" size="small" type="text" @click="clickAddPermission(row.type + 1, row.id)">添加权限点</el-button>
-              <el-button size="small" type="text">查看</el-button>
+              <el-button size="small" type="text" @click="clickEdit(row.id)">查看</el-button>
               <el-button size="small" type="text" @click="clickDel(row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
         <!-- 新增编辑的弹层 -->
-        <el-dialog :visible="showDialog" title="弹层标题" @close="showDialog = false">
+        <el-dialog :visible="showDialog" :title="showTitle" @close="closeDialog">
           <!-- 表单内容 -->
           <el-form label-width="100px">
             <el-form-item label="权限名称">
@@ -55,7 +55,7 @@
 </template>
 
 <script>
-import { reqGetPermissionList, reqAddPermission, reqDelPermission } from '@/api/permission'
+import { reqGetPermissionList, reqAddPermission, reqDelPermission, reqGetPermissionDetail, reqUpdatePermission } from '@/api/permission'
 import { transListToTreeData } from '@/utils'
 export default {
   name: 'Permission',
@@ -73,6 +73,11 @@ export default {
       }
     }
   },
+  computed: {
+    showTitle() {
+      return this.formData.id ? '查看' : '新增'
+    }
+  },
   created() {
     this.getPermissionList()
   },
@@ -86,11 +91,8 @@ export default {
       const { data } = await reqGetPermissionList()
       this.list = transListToTreeData(data, '0')
     },
-    async clickSubmit() {
-      await reqAddPermission(this.formData)
-      this.$message.success('添加权限成功')
+    closeDialog() {
       this.showDialog = false
-      this.getPermissionList()
       this.formData = {
         enVisible: '0', // 启用禁用, 0禁用, 1启用
         name: '', // 名称
@@ -99,6 +101,17 @@ export default {
         type: '', // 类型标记了一级(页面访问权) 二级(按钮操作权)
         pid: '' // 添加到哪个节点下
       }
+    },
+    async clickSubmit() {
+      this.formData.id ? (await reqUpdatePermission(this.formData)) : await reqAddPermission(this.formData)
+      this.$message.success(`${this.formData.id ? '修改' : '添加'}权限成功`)
+      this.closeDialog()
+      this.getPermissionList()
+    },
+    async clickEdit(id) {
+      this.showDialog = true
+      const { data } = await reqGetPermissionDetail(id)
+      this.formData = data
     },
     clickDel(id) {
       this.$confirm('亲，确认删除吗', '温馨提示').then(async() => {
