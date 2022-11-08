8ea90e16240d45ca63f9d3e65a4587847cae6f34
qianfengg <fred_gqf0221@163.com>
2022-11-08 11:45:09 +0800

    全屏图标准备

---
+++ src/components/ScreenFull/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg-icon icon-class="fullscreen" class="fullscreen" />
+</template>
+
+<script>
+export default {
+  name: 'ScreenFull'
+}
+</script>
+
+<style scoped lang="scss">
+.fullscreen {
+  width: 40px;
+  height: 40px;
+  fill: #fff;
+  cursor: pointer;
+}
+</style>
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -13,6 +13,7 @@
     </div>
 
     <div class="right-menu">
+      <screen-full class="right-menu-item" />
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img v-imgerror="defaultImg" :src="staffPhoto" class="user-avatar">
@@ -132,7 +133,7 @@ export default {
       height: 100%;
       font-size: 18px;
       color: #5a5e66;
-      vertical-align: text-bottom;
+      vertical-align: middle;
 
       &.hover-effect {
         cursor: pointer;
