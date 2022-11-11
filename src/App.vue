d4573005dcf78365c63603f110d52a624241e6ca
qianfengg <fred_gqf0221@163.com>
2022-11-11 11:45:38 +0800

    app添加类名控制主题色

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div id="app">
+  <div id="app" :class="classObj">
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    classObj() {
+      let className = ''
+      switch (this.$store.state.settings.theme) {
+        case '#6959CD':
+          className = 'purple-theme'
+          break
+        case '#F5222D':
+          className = 'red-theme'
+          break
+        case '#212121':
+          className = 'black-theme'
+          break
+        default:
+          className = 'default-theme'
+      }
+      return {
+        [className]: true
+      }
+    }
+  }
 }
 </script>
